0fc3e8017ff061e27a02a999ede30522e10bf79f
Jasmine Baker <jasminebro2@gmail.com>
2024-01-01 23:33:29 +0000

    Modified my contact info and package info

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Jasmine Baker, PhD",
+  title: "Bioinformatics, Data Analysis & Content Creation ",
+  email: "jasminebro2@gmail.com",
+  gitHub: "github.com/jasminebro",
+  instagram: "",
+  linkedIn: "linkedin.com/in/jasmine-baker-phd",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
