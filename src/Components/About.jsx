8835e9152e1981ae5301605a3ebafd62aa04719a
Jasmine Baker <jasminebro2@gmail.com>
2024-01-09 21:43:13 +0000

    fixed image path

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "C:\Users\12259\Downloads\laptopandnotepad.jpg";
+import image from "C://Users//12259//Downloads//laptopandnotepad.jpg";
 
 const imageAltText = "laptop and notepad";
 
